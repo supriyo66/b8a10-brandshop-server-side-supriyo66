908d8616e3349251fa553ea3c89f5524c7ab9ff4
Supriyo66 <supriyo.das3866@gmail.com>
2023-10-19 00:27:35 +0600

    get food id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -39,6 +39,13 @@ async function run() {
       const cursor=foodCollection.find();
       const result=await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('/food/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={ _id: new ObjectId(id) };
+      const result=await foodCollection.findOne(query)
+      res.send(result)
     })
 
     app.post('/food', async(req,res)=>{
